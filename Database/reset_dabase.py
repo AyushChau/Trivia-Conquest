97e7da0de5b86ffa702820865d43b4c949f06107
Ayubabe <chaudhari.ayush2001@gmail.com>
2024-04-02 01:09:32 -0600

    Dynamically updating the questions and pulling from the db

--- Database/reset_dabase.py
+++ Database/reset_dabase.py
@@ -1,10 +1,10 @@
-import pandas as pd
 import sqlite3
 
 sql_connection = sqlite3.connect('TriviaDB.db', check_same_thread=False)
 cur = sql_connection.cursor()
 
-df = pd.read_sql_query("SELECT * from trivia",sql_connection)
+query = "UPDATE trivia SET used = 0"
+cur.execute(query)
 
-print(df)
-
+sql_connection.commit()
+sql_connection.close()
--- server.py
+++ server.py
@@ -16,15 +16,25 @@ def query_db(category,num_of_questions):
     else:
         query = "Select question,answer from trivia where used == 0 ORDER BY RANDOM() Limit ?"
         cur.execute(query, (num_of_questions,))
-    
 
-    print(cur.fetchall())
+    selected_qs = cur.fetchall()
 
+    for row in selected_qs:
+        q = row[0]
+        a = row[1]
 
+        query = "UPDATE trivia SET used = 1 WHERE question = ? AND answer = ?"
+        cur.execute(query,(q,a))
+        sql_connection.commit()
+
+    return dict(selected_qs)
 
 @app.route('/')
 def homepage():
-    return render_template('home.html')
+    query = "SELECT count(*) from trivia where used == 0"
+    cur.execute(query)
+    question_count = cur.fetchone()[0]
+    return render_template('home.html',count=question_count)
 
 
 
@@ -33,11 +43,12 @@ def question_set():
     num_of_questions = request.args.get('num_of_questions')
     category = request.args.get('category')
     
-    query_db(category,num_of_questions)
-    return render_template('question.html')
+    questions = query_db(category,num_of_questions)
+    return render_template('question.html',table=questions)
 
 
 
 if __name__ == '__main__':
 
     app.run(host='0.0.0.0',port=3000,debug=True)
+    sql_connection.close()
